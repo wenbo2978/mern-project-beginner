6c13e6621705a9494e99c249fe7e4c989b65bf37
wenbo <wenbochen@1997.gmail.com>
2024-08-29 16:02:36 -0400

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGOOSE).then(()=>{
+  console.log("connect success");
+}).catch((err) => {
+  console.log(err);
+})
 
 const app = express();
 
 app.listen(3000, () => {
   console.log('server is running on 3000__!!!');
+})
+
+app.get("/test", (req, res) => {
+  res.json("test");
+  console.log("works");
 })
