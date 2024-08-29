5553025e266196a780ca71bd747f5bb2654642e5
wenbo <wenbochen@1997.gmail.com>
2024-08-29 17:00:06 -0400

    finish add&getAll api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Blog from "./models/blog.model.js";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGOOSE).then(()=>{
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('server is running on 3000__!!!');
 })
@@ -19,4 +22,26 @@ app.listen(3000, () => {
 app.get("/test", (req, res) => {
   res.json("test");
   console.log("works");
+})
+
+app.post('/add', async (req, res) => {
+  try{
+    const {title, body} = req.body;
+    //console.log(title);
+    //console.log(body);
+    const newBlog = new Blog({title, body});
+    await newBlog.save();
+    res.json('ok');
+  }catch(err){
+    res.json(err);
+  }
+})
+
+app.get('/getAll', async (req, res) => {
+  try{
+    const data = await Blog.find();
+    res.json(data);
+  }catch(err){
+    res.json(err);
+  }
 })
